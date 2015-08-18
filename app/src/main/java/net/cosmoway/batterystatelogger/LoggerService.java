1642391bdbb19b32a145d615c63dd36f651eea47
Yuuki Shimizu <shimizu@cosmoway.net>
2015-08-18 23:13:17 +0900

    LoggerService を追加／BATTERY_CHANGED ブロードキャストを受信

---
+++ app/src/main/java/net/cosmoway/batterystatelogger/LoggerService.java
@@ -0,0 +1,62 @@
+package net.cosmoway.batterystatelogger;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class LoggerService extends Service {
+
+    private BatteryStateReceiver mReceiver;
+
+    public static void startService(Context context) {
+        Intent intent = new Intent(context, LoggerService.class);
+        context.startService(intent);
+    }
+
+    public static void stopService(Context context) {
+        Intent intent = new Intent(context, LoggerService.class);
+        context.stopService(intent);
+    }
+
+    public LoggerService() {
+        mReceiver = new BatteryStateReceiver();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    private class BatteryStateReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                // バッテリーが変化した
+                Bundle bundle = intent.getExtras();
+                int temp = bundle.getInt(BatteryManager.EXTRA_TEMPERATURE);
+            }
+        }
+    }
+}
