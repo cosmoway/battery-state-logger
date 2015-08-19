5e54ee91595bb975c1e9bcdae652abcdbecde077
yuukis <yuuki@maxio.jp>
2015-08-20 08:35:05 +0900

    アプリ更新後にログ記録を自動開始する

--- app/src/main/java/net/cosmoway/batterystatelogger/AppBootReceiver.java
+++ app/src/main/java/net/cosmoway/batterystatelogger/AppBootReceiver.java
@@ -10,8 +10,9 @@ public class AppBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            // 端末起動後にログ記録を開始する
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)
+                || Intent.ACTION_MY_PACKAGE_REPLACED.equals(action)) {
+            // 端末起動後、アプリ更新後にログ記録を開始する
             LoggerService.startService(context);
         }
     }
